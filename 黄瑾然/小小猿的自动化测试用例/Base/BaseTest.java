2674ec4dd9f462f1f3682cd3e41be44080d486fe
huangjinran1999 <3029923422@qq.com>
2019-05-30 09:35:01 +0800

    自动化测试用例

---
+++ 黄瑾然/小小猿的自动化测试用例/Base/BaseTest.java
@@ -0,0 +1,35 @@
+package Base;
+
+import Page.WebDriverEngine;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import Page.WebDriverEngine;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class BaseTest{
+    AndroidDriver<WebElement> driver;
+    public WebDriverEngine webtest;
+    @BeforeSuite
+    public void startApp() throws Exception{
+        File classpathRoot = new File(System.getProperty("user.dir"));
+        File appDir = new File(classpathRoot+"/apps");
+        File app = new File(appDir.getCanonicalPath(), "Base.apk");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("deviceName", "emulator-5554");
+        capabilities.setCapability("app", app.getAbsolutePath());
+        capabilities.setCapability("appPackage", "com.google.blockly.demo");
+        capabilities.setCapability("appActivity", "com.google.blockly.android.demo.Welcome");
+        driver = new AndroidDriver<WebElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+    }
+//    @AfterSuite
+    public void tearDown(){
+        driver.quit();
+    }
+}
